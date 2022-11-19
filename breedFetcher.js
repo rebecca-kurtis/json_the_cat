1c8a2928e91e08864b8753a7355f2d34fd76e617
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-11-19 13:54:58 -0800

    adding comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,6 +4,7 @@ const args = process.argv;
 let argRequests = args.slice(2);
 let catName = argRequests[0];
 
+// Input - request api function
 const input = `https://api.thecatapi.com/v1/breeds/search?q=${catName}`;
 
 request(input, (error, response, body) => {
@@ -14,10 +15,12 @@ request(input, (error, response, body) => {
   }
 
   const data = JSON.parse(body);
+  // If breed is not found
   if (data.length === 0) {
     console.log('Cat Breed not found, try again.');
     return;
   }
+  // Print out the description only
   for (const value of data) {
     console.log(value.description);
   }
