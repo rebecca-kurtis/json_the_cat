a05e1792419b2c01bdfa3f076fa1d588655cdf4c
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-11-19 13:53:29 -0800

    adding breedFetcher.js code and eslint changes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,27 @@
 const request = require('request');
+
+const args = process.argv;
+let argRequests = args.slice(2);
+let catName = argRequests[0];
+
+const input = `https://api.thecatapi.com/v1/breeds/search?q=${catName}`;
+
+request(input, (error, response, body) => {
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log('Cat Breed not found, try again.');
+    return;
+  }
+  for (const value of data) {
+    console.log(value.description);
+  }
+
+  return;
+
+});
